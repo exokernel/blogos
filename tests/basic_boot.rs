75289a312d5e4e7fd41f2d7578d771b1d1506367
Dustin Cannon <dustin.cannon@gmail.com>
2023-07-23 23:48:29 -0700

    integration tests - should panic

--- tests/basic_boot.rs
+++ tests/basic_boot.rs
@@ -5,28 +5,22 @@
 #![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-use blogos::println;
 
-#[no_mangle]
+#[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-/// This function is called on panic.
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     blogos::test_panic_handler(info)
+}
+
+use blogos::println;
+
+#[test_case]
+fn test_println() {
+    println!("test_println output");
 }
