cccfd7533d92ed28b7ad74069342cb4bcd8f51e1
Dustin Cannon <dustin.cannon@gmail.com>
2023-07-23 20:17:24 -0700

    newlines

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -75,8 +75,6 @@ impl Writer {
         }
     }
 
-    fn new_line(&mut self) {/* TODO */}
-
     pub fn write_string(&mut self, s: &str) {
         for byte in s.bytes() {
             match byte {
@@ -86,6 +84,27 @@ impl Writer {
                 _ => self.write_byte(0xfe),
             }
 
+        }
+    }
+
+    fn new_line(&mut self) {
+        for row in 1..BUFFER_HEIGHT {
+            for col in 0..BUFFER_WIDTH {
+                let character = self.buffer.chars[row][col].read();
+                self.buffer.chars[row - 1][col].write(character);
+            }
+        }
+        self.clear_row(BUFFER_HEIGHT - 1);
+        self.column_position = 0;
+    }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
         }
     }
 }
