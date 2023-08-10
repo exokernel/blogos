95fb55af18e8043990215a465829d2d0a40cf031
Dustin Cannon <dustin.cannon@gmail.com>
2023-08-09 21:49:56 -0700

    paging implementation - OffsetPageTable

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,14 @@ use bootloader::{BootInfo, entry_point};
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use x86_64::VirtAddr;
-    use blogos::memory::translate_addr;
+    use x86_64::{structures::paging::Translate, VirtAddr};
+    use blogos::memory;
 
     println!("Hello World{}", "!");
     blogos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mapper = unsafe { memory::init(phys_mem_offset) };
 
     let addresses = [
         // the identity-mapped vga buffer page
@@ -32,7 +33,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     for &address in &addresses {
         let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        let phys = mapper.translate_addr(virt);
         println!("{:?} -> {:?}", virt, phys);
     }
 
