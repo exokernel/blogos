aecbd8828d0760ccb20c51c031607b82720aef32
Dustin Cannon <dustin.cannon@gmail.com>
2023-08-12 23:00:20 -0700

    heap allocation - allocator interface

--- src/lib.rs
+++ src/lib.rs
@@ -6,11 +6,14 @@
 #![reexport_test_harness_main = "test_main"]
 #![feature(abi_x86_interrupt)]
 
+extern crate alloc;
+
 pub mod serial;
 pub mod vga_buffer;
 pub mod interrupts;
 pub mod gdt;
 pub mod memory;
+pub mod allocator;
 
 use core::panic::PanicInfo;
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@
 #![test_runner(blogos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+extern crate alloc;
+
+use alloc::boxed::Box;
 use core::panic::PanicInfo;
 use blogos::println;
 use bootloader::{BootInfo, entry_point};
@@ -30,6 +33,8 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     // write the string `New!` to the screen through the new mapping
     let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
     unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
+    let x = Box::new(41);
 
     #[cfg(test)]
     test_main();
