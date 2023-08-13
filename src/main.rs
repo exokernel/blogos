3b7ba2349f76dd0fd502c8589f043ca8d8954210
Dustin Cannon <dustin.cannon@gmail.com>
2023-08-12 23:16:46 -0700

    heap allocation - creating a kernel heap

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,25 @@
 extern crate alloc;
 
 use alloc::boxed::Box;
-use core::panic::PanicInfo;
 use blogos::println;
-use bootloader::{BootInfo, entry_point};
+use bootloader::{entry_point, BootInfo};
+use core::panic::PanicInfo;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
+    use blogos::{memory, allocator};
     use x86_64::{structures::paging::Page, VirtAddr};
-    use blogos::memory;
 
     println!("Hello World{}", "!");
     blogos::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-    let mut frame_allocator = unsafe {
-        memory::BootInfoFrameAllocator::init(&boot_info.memory_map)
-    };
+    let mut frame_allocator =
+        unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    // map an unused page
-    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
-    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
-
-    // write the string `New!` to the screen through the new mapping
-    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
-    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
 
     let x = Box::new(41);
 
